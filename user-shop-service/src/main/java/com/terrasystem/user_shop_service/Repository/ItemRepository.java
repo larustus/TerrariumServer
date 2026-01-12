1749098fcc58e2b4ac5295beac8aecc83e1c5599
root <root@vmi2202805.contaboserver.net>
2026-01-12 14:41:03 +0100

    Remove user-shop-service from monorepo

--- user-shop-service/src/main/java/com/terrasystem/user_shop_service/Repository/ItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.terrasystem.user_shop_service.Repository;
-
-import com.terrasystem.user_shop_service.Entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> { }
--- user-shop-service/src/main/java/com/terrasystem/user_shop_service/Repository/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.terrasystem.user_shop_service.Repository;
-
-import com.terrasystem.user_shop_service.Entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> { }
